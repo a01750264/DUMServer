077208d574edf36a534d998c7ad5c030cc7ed521
a01750264 <a01750246@itesm.mx>
2021-09-01 17:35:42 -0500

    cambios iniciales en index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser')
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const puerto = 8080;
+
+app.listen(puerto, ()=>{
+    console.log(`Servidor en línea en el puerto: ${puerto}`)
+})
