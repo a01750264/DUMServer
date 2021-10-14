55381b5d2b15258c8eb617dc5e9c0331fa9f633d
a01750264 <a01750246@itesm.mx>
2021-10-13 23:27:53 -0500

    cambiando a http

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const express = require('express');
-const https = require('https');
-const fs = require('fs');
 const path = require('path');
 const bodyParser = require('body-parser')
 const dotenv = require('dotenv');
@@ -8,20 +6,16 @@ dotenv.config()
 const donanteRoutes = require('./routes/donante');
 const app = express();
 const sequelize = require('./util/db');
-const sslServer = https.createServer({
-    key: fs.readFileSync('./cert/my_cert.key'),
-    cert: fs.readFileSync('./cert/my_cert.crt')
-}, app)
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/donante', donanteRoutes);
 
-const puerto = 443;
+const puerto = 8080;
 
 sequelize.sync({force:false})
     .then(resultado=>{
         console.log("¡Conexión a base de datos exitosa!");
-        sslServer.listen(puerto, ()=>console.log(`Servidor HTTPS en línea en el puerto: ${puerto}`));
+        app.listen(puerto, ()=>console.log(`Servidor en línea en el puerto: ${puerto}`));
     })
     .catch(err=>console.log(err));
